f81d37daf092b7367e805d9e5def5bbc2d4c8c86
Julian Scharf 2015 macbook <julzelements@gmail.com>
2022-06-06 18:24:06 +1000

    save patches to json

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const transformDataFrom7BitTo8Bit = require('./utilities').transformDataFrom7BitTo8Bit;
 const Monologue = require('./monologue');
 
+const fs = require('fs')
+
+const storeData = (data, path) => {
+  try {
+    fs.writeFileSync(path, JSON.stringify(data))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
 const easymidi = require('easymidi');
 const input = new easymidi.Input('monologue KBD/KNOB');
 input.on('sysex', function (msg) {
   const data = transformDataFrom7BitTo8Bit(msg.bytes);
-  console.log(Monologue.createFromSysEx(data).toString());
+  const patch = Monologue.createFromSysEx(data)
+  const patchName = patch.patchName.split('\x00')[0]
+  console.log(`saving: ${patchName}`)
+  storeData(patch, `./patches/dutch-bass/${patchName}.json`)
 });
